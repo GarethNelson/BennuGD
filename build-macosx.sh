5644025ca48061f1230bcd642119f04165befc25
Gareth Nelson <gareth@garethnelson.com>
2016-04-03 21:46:19 +0100

    Upstream devs don't realise that autotools can autodetect the host platform

---
+++ build-macosx.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+
+
+echo "### Building BennuGD Core ###"
+
+cd core
+./configure
+make clean
+make
+cd ..
+
+echo "### Building BennuGD Modules ###"
+
+cd modules
+./configure
+make clean
+make
+cd ..
+
+echo "### Building BennuGD Tools ###"
+
+cd tools/moddesc
+./configure
+make clean
+make
+cd ..
+
+echo "### Copying files to bin folder ###"
+TARGET=macosx
+mkdir -p bin/$TARGETx 2>/dev/null
+cp core/bgdi/src/.libs/bgdi bin/$TARGET
+cp core/bgdc/src/bgdc bin/$TARGET
+cp core/bgdrtm/src/.libs/libbgdrtm.so bin/$TARGET
+cp $(find modules -name '*.so') bin/$TARGET
+cp tools/moddesc/moddesc bin/$TARGET
+
+echo "### Build done! ###"
+
+exit 0

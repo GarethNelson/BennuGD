6e34c31081cc7caca3fe5be92f8682f58aa61d4f
Gareth Nelson <gareth@garethnelson.com>
2016-04-04 04:07:16 +0100

    mode 7 demo now compiles, but segfaults after window creation

--- build-macosx.sh
+++ build-macosx.sh
@@ -34,8 +34,24 @@ cp core/bgdc/src/bgdc bin/$TARGET
 cp core/bgdrtm/src/.libs/libbgdrtm.dylib bin/$TARGET
 cp $(find modules -name '*.dylib') bin/$TARGET
 cp tools/moddesc/moddesc bin/$TARGET
+
+echo "### Fixing dodgy linker ###"
 install_name_tool -change /usr/local/lib/libbgdrtm.dylib @executable_path/libbgdrtm.dylib bin/macosx/bgdi
 find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libbgdrtm.dylib @executable_path/libbgdrtm.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/librender.dylib @executable_path/librender.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libvideo.dylib @executable_path/libvideo.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libblit.dylib @executable_path/libblit.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libgrbase.dylib @executable_path/libgrbase.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libtext.dylib @executable_path/libtext.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libmap.dylib @executable_path/libmap.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libdraw.dylib @executable_path/libdraw.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libmath.dylib @executable_path/libmath.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libgrproc.dylib @executable_path/libgrproc.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libkey.dylib @executable_path/libkey.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libwm.dylib @executable_path/libwm.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libfont.dylib @executable_path/libfont.dylib {}
+find bin/macosx/*.dylib | xargs -n 1 -I{} install_name_tool -change /usr/local/lib/libbgload.dylib @executable_path/libbgload.dylib {}
+
 
 echo "### Build done! ###"
 
